46b486f590117f6e787bff77074a527875502ee8
Adam Stępień <vesemir2k@gmail.com>
2024-06-04 01:46:10 +0200

    ✨ (tailwind.config.js): add new font sizes for headings and body text to improve typography styling in the application

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,14 @@ export default {
     fontFamily: {
       plusJakartaSans: ['Plus Jakarta Sans', 'sans-serif'],
     },
+    fontSize: {
+      headingXL: ['56px', { lineHeight: '70px', fontWeight: '800' }],
+      headingL: ['40px', { lineHeight: '51px', fontWeight: '800' }],
+      headingM: ['32px', { lineHeight: '40px', fontWeight: '800' }],
+      headingS: ['24px', { lineHeight: '28px', fontWeight: '800' }],
+      bodyM: ['18px', { lineHeight: '28px', fontWeight: '400' }],
+      bodyS: ['16px', { lineHeight: '28px', fontWeight: '700' }],
+    },
     extend: {},
   },
   plugins: [],
