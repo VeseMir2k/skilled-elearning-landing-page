1a60d20096b9301a4b0a0699102d29d0f46f3411
Adam Stępień <vesemir2k@gmail.com>
2024-06-03 22:41:04 +0200

    🔧 (prettierrc): add .prettierrc configuration file with specified formatting rules for Prettier ⬆️ (package.json): upgrade prettier to version 3.3.0 and add prettier-plugin-tailwindcss as a dependency ♻️ (App.tsx): refactor App component to include underline styling for the text

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import "./App.css";
 
-const App = () => <h1 className="text-3xl font-bold">Hello world!</h1>;
+const App = () => (
+  <h1 className="text-3xl font-bold underline">Hello world!</h1>
+);
 export default App;
