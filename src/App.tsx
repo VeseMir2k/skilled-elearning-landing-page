d21d48e6fabd9c3fda2f90bf1494e55c37547397
Adam Stępień <vesemir2k@gmail.com>
2024-06-04 02:50:26 +0200

    📝 (App.tsx): remove unnecessary parentheses and line breaks for cleaner code ♻️ (tailwind.config.js): add background image gradients for better styling options

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import "./App.css";
+import './App.css';
 
-const App = () => (
-  <h1 className="text-3xl font-bold underline">Hello world!</h1>
-);
+const App = () => <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,12 @@ export default {
       bodyM: ['18px', { lineHeight: '28px', fontWeight: '400' }],
       bodyS: ['16px', { lineHeight: '28px', fontWeight: '700' }],
     },
+    backgroundImage: () => ({
+      gradient_1: 'linear-gradient(180deg, rgba(255,111,72,1) 0%, rgba(240,42,166,1) 100%)',
+      gradientHover_1: 'linear-gradient(180deg, rgba(255,111,72,.5) 0%, rgba(240,42,166,.5) 100%)',
+      gradient_2: 'linear-gradient(180deg, rgba(72,81,255,1) 0%, rgba(240,42,166,1) 100%)',
+      gradientHover_2: 'linear-gradient(180deg, rgba(72,81,255,.5) 0%, rgba(240,42,166,.5) 100%)',
+    }),
     extend: {},
   },
   plugins: [],
