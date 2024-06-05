5fa679f4dbbb0e9c70d5b6c991187f6ec8c74138
Adam Stępień <vesemir2k@gmail.com>
2024-06-06 00:11:31 +0200

    ✨ (public/assets): add new SVG icons for different categories to enhance visual representation on the website 📝 (src/components/courses/Course.tsx): refactor Course component to accept name, description, and icon props for better reusability and flexibility
    ✨ (Courses.tsx): Add icons to each course item for better visual representation and user experience.

--- src/components/courses/Course.tsx
+++ src/components/courses/Course.tsx
@@ -1,12 +1,8 @@
-import animationIcon from '../../assets/icon-animation.svg';
-
-const Course = () => (
+const Course = ({ name, description, icon }: { name: string; description: string; icon: string }) => (
   <div className="relative rounded-[10px] bg-white px-[28px]">
-    <img className="absolute top-[-28px]" src={animationIcon} alt="animation" />
-    <h4 className="text-navyBlue mt-[56px] text-[20px] font-extrabold">Animation</h4>
-    <p className="text-grayishBlue text-bodyS mt-[16px] font-normal">
-      Learn the latest animation techniques to create stunning motion design and captivate your audience.
-    </p>
+    <img className="absolute top-[-28px]" src={icon} alt="animation" />
+    <h4 className="text-navyBlue mt-[56px] text-[20px] font-extrabold">{name}</h4>
+    <p className="text-grayishBlue text-bodyS mt-[16px] font-normal">{description} </p>
     <button className="text-bodyM text-hotPink mb-[32px] mt-[24px] font-bold">Get Started</button>
   </div>
 );
--- src/components/courses/Courses.tsx
+++ src/components/courses/Courses.tsx
@@ -6,28 +6,35 @@ const Courses = () => {
       name: 'Animation',
       description:
         'Learn the latest animation techniques to create stunning motion design and captivate your audience.',
+      icon: './assets/icon-animation.svg',
     },
     {
       name: 'Design',
       description: 'Create beautiful, usable interfaces to help shape the future of how the web looks.',
+      icon: './assets/icon-design.svg',
     },
     {
       name: 'Photography',
       description: 'Explore critical fundamentals like lighting, composition, and focus to capture exceptional photos.',
+      icon: './assets/icon-photography.svg',
     },
     {
       name: 'Crypto',
       description:
         'All you need to know to get started investing in crypto. Go from beginner to advanced with this 54 hour course.',
+      icon: './assets/icon-crypto.svg',
     },
     {
       name: 'Business',
       description:
         'A step-by-step playbook to help you start, scale, and sustain your business without outside investment.',
+      icon: './assets/icon-business.svg',
     },
   ];
 
-  const courses = coursesData.map(() => <Course />);
+  const courses = coursesData.map((item) => (
+    <Course name={item.name} description={item.description} icon={item.icon} />
+  ));
 
   return (
     <div className="bg-gradient_3 flex flex-col gap-[44px]">
