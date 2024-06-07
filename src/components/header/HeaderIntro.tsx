9ed8a583424eea02af0348693b643e2a5fa68522
Adam Stępień <vesemir2k@gmail.com>
2024-06-07 22:07:51 +0200

    ✨ (HeaderIntro.tsx): Increase font size and adjust styling for better readability and visual appeal in the HeaderIntro component

--- src/components/header/HeaderIntro.tsx
+++ src/components/header/HeaderIntro.tsx
@@ -2,8 +2,8 @@ import Button from '../button';
 
 const HeaderIntro = () => (
   <div className="w-[60%]">
-    <h2 className="text-headingL text-navyBlue pb-[26px]">Maximize skill, minimize budget</h2>
-    <p className="text-grayishBlue pb-[24px] text-[16px] font-normal leading-[26px]">
+    <h2 className="text-headingL text-navyBlue lg:text-headingXL pb-[26px]">Maximize skill, minimize budget</h2>
+    <p className="text-grayishBlue lg:text-bodyM pb-[24px] text-[16px] font-normal leading-[26px]">
       Our modern courses across a range of in-demand skills will give you the knowledge you need to live the life you
       want.
     </p>
